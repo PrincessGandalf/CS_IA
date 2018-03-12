d23d143423399ad25db898e3def2b48b149ec674
hallyb <thespeedygenius@gmail.com>
2018-03-11 22:05:45 -0400

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -1,4 +1,30 @@
 
 public class Hotel {
+	private String room;
+	private String roomType;
+	
+	public Hotel(String r, String rt) {
+		room = r;
+		setRoomType(rt);
+	}
+	
 
+	public String getRoom() {
+		return room;
+	}
+
+	public void setRoom(String room) {
+		this.room = room;
+	}
+
+
+	public String getRoomType() {
+		return roomType;
+	}
+
+
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+	
 }
